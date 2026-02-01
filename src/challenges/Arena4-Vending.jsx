12a684d6b1250646a48f0c33000f36846247893f
Kian Davey <kiandaveyantolin@gmail.com>
2026-02-01 22:05:15 +0800

    Started Arena 4: Vending

--- src/challenges/Arena4-Vending.jsx
+++ src/challenges/Arena4-Vending.jsx
@@ -4,6 +4,21 @@ export default function Arena4() {
         <div>
             <h2 style={{color: 'orange'}}>Arena Challenge 4: Vending</h2>
 
+            <div>
+                <p>Current Balance: $</p>
+                <button>Insert 1$</button>
+                <button>Refund</button>
+            </div>
+
+            <div>
+                <p>Inventory: </p>
+                <p>Name: </p>
+                <p>Price: </p>
+                <p>Stock Count: </p>
+            </div>
+
+            <p>Dispensing [] ...</p>
+
         </div>
     );
 }
