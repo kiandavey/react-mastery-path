531e9c0c033bd77d397c30120e51cd177e81159b
Kian Davey <kiandaveyantolin@gmail.com>
2026-02-05 21:50:30 +0800

    Declared states and connected to API

--- src/challenges/Arena5-Feed.jsx
+++ src/challenges/Arena5-Feed.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
 
 export default function Arena5() {
+
+    const [page, setPage] = useState(1);
+
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState()
+
+    useEffect(() => {
+        setPage(null);
+
+        fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`)
+            .then(response => response.json())
+            .then(data => {
+                setPage(data);
+            });
+    }, [page]);
 
     return (
         <div></div>
